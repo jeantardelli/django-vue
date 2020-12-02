32fb61ff1927aae1bd300429c7ce05132f125a72
jeantardelli <jeantardelli@gmail.com>
2020-12-02 22:47:45 +0100

    Django Models (1a)

---
+++ README.md
@@ -0,0 +1,7 @@
+## Full Stack Development 101
+
+This repo prototype a custom web app using Django and Vue.js. This is an introductory hands-on project that I'll be learning about Django and Vue.js
+
+## Python + JavaScript
+
+I'll set a publishing project with a basic database schema to save bands and albumns, and a minimal interface (UI) to interact with them.
---
+++ djangovue/app/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Band(models.Model):
+    """Table schema to store a band."""
+    name = models.CharField(max_length=64)
+    area = models.CharField(max_lenght=64)
+    genre = models.CharField(max_lenght=64)
+    date_formed = models.DateTimeField()
+
+    def __repr__(self):
+        return '<Name %s>' % self.name
+
+class Album(models.Model):
+    """Table schema to store albumns."""
+    name = models.CharField(max_lenght=64)
+    author = models.ForeignKey('app.Band', on_delete=models.CASCADE)
+    label = models.CharField(max_lenght=64)
+    release = models.DateTimeField()
+
+    def __repr__(self):
+        return '<Name %s>' % self.name
